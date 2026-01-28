9789e92153edaeace1c230359ace87ae220cc718
S-Prithinga-SML <prithinga.senthilkumar@sharpmindlabs.com>
2026-01-28 17:28:22 +0530

    Operator Assignment and Arithmetic

---
+++ arithmetic.js
@@ -0,0 +1,80 @@
+//addtion operation
+let a = 10;
+let b = 20;
+let c = a + b;
+console.log(c);
+
+
+let x=15;
+let y=25;
+console.log(x+y);
+
+
+let firstName="Prithinga";
+let lastNaeme="Senthilkumar";
+console.log(firstName + " " + lastNaeme);
+
+//subtraction operation
+let d = 30;
+let e = 10;
+let f = d - e;
+console.log(f);
+
+let p="50";
+let q="20";
+console.log(p - q);
+
+let m="100";
+let n=40;
+console.log(m - n);
+
+//multiplication operation
+let g = 5;
+let h = 4;
+let i = g * h;
+console.log(i);
+
+let r=7;
+let s=3;
+console.log(r * s); 
+
+let u="6";
+let v=2;
+console.log(u * v);
+
+
+let num1="20"
+let num2="5"
+console.log(num1 * num2);
+
+
+//division operation
+let j = 20;
+let k = 5;
+let l = j / k;
+console.log(l); 
+
+let w=40;
+let z=8;
+console.log(w / z);
+
+let alpha="100";
+let beta=4;
+console.log(alpha / beta);
+
+
+//modulus operation
+
+console.log(10 % 3);
+console.log(7% 40);
+
+
+//increment operation
+let incrementValue = 10;
+incrementValue++;
+console.log(incrementValue);
+
+//decrement operation
+let decrementValue = 10;
+decrementValue--;
+console.log(decrementValue);
---
+++ assignment.js
@@ -0,0 +1,63 @@
+//assigning a value
+let a=10;
+console.log(a);
+
+//Addition Assignment Operator
+let b=25;
+let c=12;
+b+=c;
+console.log(b);
+
+//Subtraction Assignment Operator
+let d=50;
+d-=20;
+console.log(d);
+
+//Multiplication Assignment Operator
+let e=20;
+e*=3;
+console.log(e);
+
+//Division Assignment Operator
+let f=100;
+f/=4;
+console.log(f);
+
+//modulus assignment operator
+let g=29;
+g%=5;
+console.log(g);
+
+//Logical AND Assignment
+let x = true;
+let y = x &&= 10;
+console.log(y);
+
+let m = false;
+let n = m &&= 10;
+console.log(n);
+
+let h= 1;
+let i = h &&= 10;
+console.log(i);
+
+let hh= 0;
+let ii = hh &&= 111;
+console.log(ii);
+
+//Logical OR Assignment
+let p = false;
+let q = p ||= 20;
+console.log(q);
+
+let r = true;
+let s = r ||= 30;
+console.log(s);
+
+let xx=null;
+let yy = xx ||= 40;
+console.log(yy);
+
+let aa=undefined;
+let bb = aa ||= 50;
+console.log(bb);
