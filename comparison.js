9180aff947bdc1f11e479e2fdc4f52fb98818478
S-Prithinga-SML <prithinga.senthilkumar@sharpmindlabs.com>
2026-01-29 22:03:16 +0530

    Add comparison and logical operator files

---
+++ comparison.js
@@ -0,0 +1,40 @@
+a=10
+b="10"
+console.log("Equal to " + (a == b));  
+console.log("Strictly Equal to " + (a === b)); 
+console.log("Not Equal to " + (a != b));
+console.log("Strictly Not Equal to " + (a !== b)); 
+console.log("Greater than " + (a > b));
+console.log("Less than " + (a < b));
+console.log("Greater than or Equal to " + (a >= b));
+console.log("Less than or Equal to " + (a <= b));
+console.log("Logical AND " + ((a === 10) && (b === "10")));
+console.log("Logical OR " + ((a === 10) || (b === 5))); 
+console.log("Logical NOT " + !(a === b));
+console.log("Logical NOT " + !(a !== b));
+
+c=""
+d=9;
+console.log("Logical AND " + (c && d));
+console.log("Logical OR " + (c || d));  
+console.log("Logical NOT " + !c);   
+console.log("Equal to " + (c == d));
+console.log("Not Equal to " + (c != d));    
+console.log("Strictly Equal to " + (c === d));
+console.log("Strictly Not Equal to " + (c !== d));
+console.log("Greater than " + (c > d));
+console.log("Less than " + (c < d));
+console.log("Greater than or Equal to " + (c >= d));
+console.log("Less than or Equal to " + (c <= d));
+
+e=null;
+f=5;
+console.log("Logical AND " + (e && f)); 
+console.log("Logical OR " + (e || f));  
+console.log("Logical NOT " + !e);   
+console.log("Equal to " + (e == f));    
+console.log("Not Equal to " + (e != f));
+console.log("Strictly Equal to " + (e === f));
+console.log("Strictly Not Equal to " + (e !== f));
+console.log("Greater than " + (e > f));
+console.log("Less than " + (e < f));
---
+++ logical.js
@@ -0,0 +1,51 @@
+a=10
+b="10"
+console.log("Greater than " + (a > b));
+console.log("Less than " + (a < b));
+console.log("Greater than or Equal to " + (a >= b));
+console.log("Less than or Equal to " + (a <= b));
+console.log("Logical AND " + ((a === 10) && (b === "10")));
+console.log("Logical OR " + ((a === 10) || (b === 5))); 
+
+c="";
+d="Hello"
+c && d
+
+c;
+d="Hello"
+c && d
+
+c=null;
+d="Hello"
+c && d
+
+c=0;
+d="Hello"
+c && d
+
+c=null;
+d="Hello"
+c && d
+
+c="";
+d="Hello"
+c || d
+
+c;
+d="Hello"
+c || d
+
+c=null;
+d="Hello"
+c || d
+
+c=0;
+d="Hello"
+c || d
+
+c=null;
+d="Hello"
+c || d
+
+
+    
