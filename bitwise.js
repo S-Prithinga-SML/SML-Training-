a4f1604c623873e95e9e8a0042b8c92d5098970e
S-Prithinga-SML <prithinga.senthilkumar@sharpmindlabs.com>
2026-01-30 04:34:54 +0530

    Update bitwise.js

--- bitwise.js
+++ bitwise.js
@@ -40,13 +40,12 @@ if(loggedIn && serverUp){
     console.log("Please login again or try later")
 }
 
-a=10;
-b=2;
-console.log("Logical AND " + (a && b));  
-console.log("Logical OR " + (a || b));  
-console.log("Xor logic " + (a ^ b));
-console.log("~Xor logic " + ~(a ^ b));
-
+m=10;
+n=2;
+console.log("Logical AND " + (m && n));  
+console.log("Logical OR " + (m || n));  
+console.log("Xor logic " + (m ^ n));
+console.log("~Xor logic " + ~(m ^ n));                      
 
 //lexiographic comparison
 str1="apple"
@@ -62,3 +61,37 @@ console.log("Bitwise AND " + (a & b));
 console.log("Bitwise OR " + (a | b));
 console.log("Bitwise XOR " + (a ^ b));
 console.log("Bitwise NOT a " + (~a));
+
+
+let p = [1, 2, 3];
+let q = [...p, 4, 5];
+console.log(q);
+
+
+//let myName=["Prithinga","Senthilkumar"];
+//let fullName=[...myName];
+//console.log(fullName);
+
+
+//comma operator
+let x= (2,3,4,5);
+console.log(x);
+
+let y;
+y=(10,20,30,40);
+console.log(y);
+
+
+let r=(1+2, 3+4, 5+6);
+let s=r+10;
+console.log(s);            
+
+let o=(2,4,6,8)
+let t=(6,4,1)
+console.log(o,t)
+console.log(o+t)
+
+
+z=( (1+2), (3+4), (5+6) );
+k=(10+o);
+console.log(k);
